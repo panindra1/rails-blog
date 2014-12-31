3f16a5880f92d4cb07c8e9a9f7a2e98aafa82278
panindra1 <tumkurs2@illinois.edu>
2014-12-31 10:40:32 -0600

    added associations and validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230180526) do
+ActiveRecord::Schema.define(version: 20141231035346) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
